460799c9930e8629c3b1a9704d8ea389e275aab6
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-27 23:00:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ var array = [];
 function forLoop(array){
   for(let i=0; i<25; i++){
     if(i===1) {
-      array.push(`I am a ${i} strange loop.`);
+      array.push(`"I am a ${i} strange loop."`);
     return array;
     }
     else {
